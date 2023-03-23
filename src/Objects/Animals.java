5f14e4b3c755628743246a19b7356d1bfaee4c7b
reky <me@reky.dev>
2023-03-23 08:37:41 +0700

    Add objects

---
+++ src/Objects/Animals.java
@@ -0,0 +1,8 @@
+package Objects;
+
+public class Animals extends AbstractObject {
+    public static String[] OptionsInputMessages = {"Name: ", "Habitat: "};
+    public Animals() {
+        super(OptionsInputMessages);
+    }
+}
